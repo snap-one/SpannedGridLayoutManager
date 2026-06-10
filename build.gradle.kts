c865c02233cf45a48ad04310f462751223951367
andrew.turner <andrew.turner@snapone.com>
2026-06-10 11:05:08 -0600

    convert gradle files to kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
-    id("maven-publish")
+    `maven-publish`
 }
---
+++ spannedgridlayoutmanager/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.parcelize)
+    `maven-publish`
+}
+
+android {
+    namespace = "com.arasthel.spannedgridlayoutmanager"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 21
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.recyclerview)
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.snap-one"
+            artifactId = "SpannedGridLayoutManager"
+            version = "4.0.5"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
